72a527c07d8070035a7b01d79eb99f3cfd0152f6
Angelo Genovese <angelo@streetcontxt.com>
2020-03-10 20:30:54 -0400

    Add TODOs for next steps

--- src/main/java/neuralnerdwork/NeuralNetworkTrainer.java
+++ src/main/java/neuralnerdwork/NeuralNetworkTrainer.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class NeuralNetworkTrainer {
     NeuralNetwork train(List<TrainingSample> samples) {
+        // TODO - Build a model of a neuron
+        // TODO - Build a model of layers of neurons
+        // TODO - Find the derivatives of a weight of a neuron
+        // TODO - Update neuron weights based on gradient and learning parameter
+        // TODO - Stop when we have converged
         return new NeuralNetwork();
     }
 }
