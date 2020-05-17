fcca43bc41b36b5c60fc6a2496f30db730366f22
Max Barkley <max.barkley@gmail.com>
2020-05-17 15:05:57 -0400

    Add single step gradient descent test for backprop implementation

--- src/test/java/neuralnerdwork/GradientDescentTest.java
+++ src/test/java/neuralnerdwork/GradientDescentTest.java
@@ -13,20 +13,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GradientDescentTest {
 
-    private static final int singleDescentStep_rows = 10;
-    private static final int singleDescentStep_cols = 10;
-    private static final int singleDescentStep_layers = 50;
-    private static final int singleDescentStep_parameters =
-            singleDescentStep_rows
-                    * (singleDescentStep_cols + 1)
-                    * singleDescentStep_layers
-                    + singleDescentStep_cols;
+    private static final int singleDescentStepWithGenericDerivativeShouldReduceError_rows = 10;
+    private static final int singleDescentStepWithGenericDerivativeShouldReduceError_cols = 10;
+    private static final int singleDescentStepWithGenericDerivativeShouldReduceError_layers = 10;
+    private static final int singleDescentStepWithGenericDerivativeShouldReduceError_parameters =
+            singleDescentStepWithGenericDerivativeShouldReduceError_rows
+                    * (singleDescentStepWithGenericDerivativeShouldReduceError_cols + 1)
+                    * singleDescentStepWithGenericDerivativeShouldReduceError_layers
+                    + singleDescentStepWithGenericDerivativeShouldReduceError_cols;
     @Property(tries = 3, shrinking = ShrinkingMode.OFF)
-    void singleDescentStep(@ForAll @Size(value = singleDescentStep_parameters) @Weight double[] values,
-                           @ForAll @TrainingOutput @Size(singleDescentStep_rows) double[] outputValues) {
-        final int rows = singleDescentStep_rows;
-        final int cols = singleDescentStep_cols;
-        final int numLayers = singleDescentStep_layers;
+    void singleDescentStepWithGenericDerivativeShouldReduceError(@ForAll @Size(value = singleDescentStepWithGenericDerivativeShouldReduceError_parameters) @Weight double[] values,
+                           @ForAll @TrainingOutput @Size(singleDescentStepWithGenericDerivativeShouldReduceError_rows) double[] outputValues) {
+        final int rows = singleDescentStepWithGenericDerivativeShouldReduceError_rows;
+        final int cols = singleDescentStepWithGenericDerivativeShouldReduceError_cols;
+        final int numLayers = singleDescentStepWithGenericDerivativeShouldReduceError_layers;
 
         final Model builder = new Model();
         final VectorExpression trainingInput = builder.createParameterVector(cols);
@@ -64,6 +64,65 @@ public class GradientDescentTest {
         Arrays.fill(ones, 1.0);
         final ScalarExpression squaredError = new DotProduct(new ConstantVector(ones),
                 new VectorizedSingleVariableFunction(new SquaredSingleVariableFunction(), error));
+
+        final VectorExpression lossDerivative = Util.logTiming("Computed derivative function", () -> squaredError.computeDerivative(
+                Arrays.copyOfRange(builder.variables(), trainingInput.length(), builder.size())
+        ));
+
+        final Model.Binder binder = builder.createBinder();
+        final int[] vars = binder.variables();
+        for (int i = 0; i < vars.length; i++) {
+            binder.put(vars[i], values[i]);
+        }
+        System.out.println();
+        final double originalError = Util.logTiming("Invoked undifferentiated error", () -> squaredError.evaluate(binder));
+        Util.logFunctionStructure(lossDerivative);
+        final Vector updatedWeights = Util.logTiming("Applied derivative function", () -> lossDerivative.evaluate(binder));
+        final double learningRate = 0.01;
+        for (int i = 0; i < updatedWeights.length(); i++) {
+            final int weightVariableIndex = trainingInput.length() + i;
+            binder.put(weightVariableIndex, binder.get(weightVariableIndex) - learningRate * updatedWeights.get(i));
+        }
+        final double updated = squaredError.evaluate(binder);
+        assertTrue(updated <= originalError, String.format("Expected error to decrease, but observed (original, updated) = (%f, %f)", originalError, updated));
+        System.out.println();
+    }
+
+    private static final int singleDescentStepWithBackpropDerivativeShouldReduceError_rows = 10;
+    private static final int singleDescentStepWithBackpropDerivativeShouldReduceError_cols = 10;
+    private static final int singleDescentStepWithBackpropDerivativeShouldReduceError_layers = 50;
+    private static final int singleDescentStepWithBackpropDerivativeShouldReduceError_parameters =
+            singleDescentStepWithBackpropDerivativeShouldReduceError_rows
+            * (singleDescentStepWithBackpropDerivativeShouldReduceError_cols + 1)
+            * singleDescentStepWithBackpropDerivativeShouldReduceError_layers
+            + singleDescentStepWithBackpropDerivativeShouldReduceError_cols;
+    @Property(tries = 3, shrinking = ShrinkingMode.OFF)
+    void singleDescentStepWithBackpropDerivativeShouldReduceError(@ForAll @Size(value = singleDescentStepWithBackpropDerivativeShouldReduceError_parameters) @Weight double[] values,
+                                                                 @ForAll @TrainingOutput @Size(singleDescentStepWithBackpropDerivativeShouldReduceError_rows) double[] outputValues) {
+        final int rows = singleDescentStepWithBackpropDerivativeShouldReduceError_rows;
+        final int cols = singleDescentStepWithBackpropDerivativeShouldReduceError_cols;
+        final int numLayers = singleDescentStepWithBackpropDerivativeShouldReduceError_layers;
+
+        final Model builder = new Model();
+        final ConstantVector trainingInput = new ConstantVector(Arrays.copyOf(values, cols));
+
+        final LogisticFunction logistic = new LogisticFunction();
+
+        final FeedForwardNetwork.Layer[] layers = new FeedForwardNetwork.Layer[singleDescentStepWithBackpropDerivativeShouldReduceError_layers];
+        for (int i = 0; i < numLayers; i++) {
+            ParameterMatrix weights = builder.createParameterMatrix(rows, cols + 1);
+            layers[i] = new FeedForwardNetwork.Layer(weights, logistic);
+        }
+
+        final FeedForwardNetwork networkFunction = new FeedForwardNetwork(trainingInput, layers);
+
+        final ConstantVector trainingOutput = new ConstantVector(outputValues);
+        final VectorSum error = new VectorSum(networkFunction, new ScaledVector(-1.0, trainingOutput));
+
+        final double[] ones = new double[rows];
+        Arrays.fill(ones, 1.0);
+        final ScalarExpression squaredError = new DotProduct(new ConstantVector(ones),
+                                                             new VectorizedSingleVariableFunction(new SquaredSingleVariableFunction(), error));
 
         final VectorExpression lossDerivative = Util.logTiming("Computed derivative function", () -> squaredError.computeDerivative(
                 Arrays.copyOfRange(builder.variables(), trainingInput.length(), builder.size())
